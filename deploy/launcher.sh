5188e76af28ab2e010bcb0309c95bc0dbd98f4e1
AreSZerA <18722064@bjtu.edu.cn>
2022-04-14 10:38:00 +0800

    Tidy launcher script

--- deploy/launcher.sh
+++ deploy/launcher.sh
@@ -1,254 +1,211 @@
 #!/usr/bin/env bash
 
-function show_helps() {
-  echo 'Usage: launcher.sh [OPTION]
+ORDERER="orderer1.dl4csr.org:7050"
+PK="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem"
+CU_CHANNEL="claytonuniversitychannel"
+GU_CHANNEL="garytonuniversitychannel"
+CU_CLI="cli.clayton-university.dl4csr.org"
+GU_CLI="cli.garyton-university.dl4csr.org"
 
-The options are:
+INIT_ARGS='{"Args":["init"]}'
+TEST_ARGS='{"Args":["ping"]}'
+
+COUNT_STEP=1
+
+function confirm() {
+  read -r -n 1 -p "$1 [y/N] " input
+  echo
+  if [ "$input" != "y" ]; then
+    echo "Operation canceled"
+    echo
+    exit 0
+  fi
+}
+
+function print_step() {
+  echo -e "\n\033[32m>>> Step ${COUNT_STEP}: $1\033[0m"
+  ((COUNT_STEP++))
+}
+
+function print_helps() {
+  echo 'Usage: launcher.sh [COMMAND]
+
+The commands are:
   -h, --help       Show this help
   -d, --deploy     Deploy or redeploy the network, blockchain will be wiped then recreated
   -r, --remove     Remove the network, volumes will be deleted and data will be wiped
   -s, --start      Start the existing network, the blockchain will be restored
   -p, --pause      Stop the existing network, the blockchain still exists in containers
   -u, --upgrade    Upgrade chaincode, use with a new version number
-  -i, --invoke     Function invocation test of the chaincode, use with function name and arguments
-'
+  -i, --invoke     Function invocation test of the chaincode, use with function name and arguments'
 }
 
-function deploy() {
-  read -r -n 1 -p 'Data will lost if the network has been deployed before, still deploy? [y/N] ' FLAG
-  echo
-  if [ "$FLAG" != "y" ]; then
-    echo "Operation canceled"
-    exit 0
-  fi
-  echo '====================================================================================
-  oooooooooo.    ooooo               .o      .oooooo.     .oooooo..o  ooooooooo.
-  `888'"'"'   `Y8b   `888'"'"'             .d88     d8P'"'"'  `Y8b   d8P'"'"'    `Y8  `888   `Y88.
-   888      888   888            .d'"'"'888    888           Y88bo.        888   .d88'"'"'
-   888      888   888          .d'"'"'  888    888            `"Y8888o.    888ooo88P'"'"'
-   888      888   888          88ooo888oo  888                `"Y88b   888`88b.
-   888     d88'"'"'   888       o       888    `88b    ooo   oo     .d8P   888  `88b.
-  o888bood8P'"'"'    o888ooooood8      o888o    `Y8bood8P'"'"'   8""88888P'"'"'   o888o  o888o
-===================================================================================='
+function print_logo() {
+  echo '
+======================================================================================
+   oooooooooo.    ooooo               .o      .oooooo.     .oooooo..o  ooooooooo.
+   `888'"'"'   `Y8b   `888'"'"'             .d88     d8P'"'"'  `Y8b   d8P'"'"'    `Y8  `888   `Y88.
+    888      888   888            .d'"'"'888    888           Y88bo.        888   .d88'"'"'
+    888      888   888          .d'"'"'  888    888            `"Y8888o.    888ooo88P'"'"'
+    888      888   888          88ooo888oo  888                `"Y88b   888`88b.
+    888     d88'"'"'   888       o       888    `88b    ooo   oo     .d8P   888  `88b.
+   o888bood8P'"'"'    o888ooooood8      o888o    `Y8bood8P'"'"'   8""88888P'"'"'   o888o  o888o
+======================================================================================'
+}
 
-  STEP_BEG="\\033[32m"
-  STEP_END="\\033[0m"
-  COUNT_STEP=1
+function deploy_network() {
+  print_step "Deploy network"
+  docker-compose up -d
+  echo "Sleep 10 seconds for kafka cluster to complete booting..."
+  sleep 10
+}
 
-  function print_step() {
-    echo -e "\n${STEP_BEG}>>> Step ${COUNT_STEP}: $1${STEP_END}"
-    ((COUNT_STEP++))
-  }
+function start_network() {
+  print_step "Start network"
+  docker-compose start
+}
 
-  export PATH=${PWD}/bin:${PWD}:$PATH
+function stop_network() {
+  print_step "Stop network"
+  docker-compose stop
+}
 
-  print_step "Clean environment"
-  sudo rm -rf ./channel-artifacts
-  sudo rm -rf ./crypto-config
-  mkdir -p ./channel-artifacts
-  mkdir -p ./crypto-config
-  docker-compose down --remove-orphans
-  docker system prune --volumes -f
-  for image in $images; do
-    docker rmi -f "$image"
-  done
+function create_channels() {
+  print_step "Create channels"
+  CU_TX="/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/$CU_CHANNEL.tx"
+  GU_TX="/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/$GU_CHANNEL.tx"
+  docker exec "$CU_CLI" peer channel create -o "$ORDERER" -c "$CU_CHANNEL" -f "$CU_TX" --tls --cafile "$PK"
+  docker exec "$GU_CLI" peer channel create -o "$ORDERER" -c "$GU_CHANNEL" -f "$GU_TX" --tls --cafile "$PK"
+}
 
+function join_channels() {
+  print_step "Join channels"
+  docker exec "$CU_CLI" peer channel join -b "$CU_CHANNEL".block --tls --cafile "$PK"
+  docker exec "$GU_CLI" peer channel join -b "$GU_CHANNEL".block --tls --cafile "$PK"
+}
+
+function generate_files() {
   print_step "Generates certifications, genesis block, and channel configuration transactions"
+  mkdir ./channel-artifacts
+  mkdir ./crypto-config
+  export PATH=${PWD}/bin:${PWD}:$PATH
   cryptogen generate --config=./crypto-config.yaml
-  configtxgen \
-    -profile TwoOrgsOrdererGenesis \
-    -outputBlock ./channel-artifacts/genesis.block \
-    -channelID genesischannel
-  configtxgen \
-    -profile TwoOrgsChannel \
-    -outputCreateChannelTx ./channel-artifacts/claytonuniversitychannel.tx \
-    -channelID claytonuniversitychannel
-  configtxgen \
-    -profile TwoOrgsChannel \
-    -outputCreateChannelTx ./channel-artifacts/garytonuniversitychannel.tx \
-    -channelID garytonuniversitychannel
+  configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block -channelID genesischannel
+  configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/"$CU_CHANNEL".tx -channelID "$CU_CHANNEL"
+  configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/"$GU_CHANNEL".tx -channelID "$GU_CHANNEL"
   CLAYTON_UNIVERSITY_CA_PK=$(cd crypto-config/peerOrganizations/clayton-university.dl4csr.org/ca && ls *_sk)
   GARYTON_UNIVERSITY_CA_PK=$(cd crypto-config/peerOrganizations/garyton-university.dl4csr.org/ca && ls *_sk)
   export CLAYTON_UNIVERSITY_CA_PK
   export GARYTON_UNIVERSITY_CA_PK
-
-  print_step "Start network"
-  docker-compose up -d
-  echo "Sleep 10 seconds for kafka cluster to complete booting..."
-  sleep 10
-
-  print_step "Create and join channels"
-  docker exec cli.clayton-university.dl4csr.org peer channel create \
-    -o orderer1.dl4csr.org:7050 \
-    -c claytonuniversitychannel \
-    -f /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/claytonuniversitychannel.tx \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.garyton-university.dl4csr.org peer channel create \
-    -o orderer1.dl4csr.org:7050 \
-    -c garytonuniversitychannel \
-    -f /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/garytonuniversitychannel.tx \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.clayton-university.dl4csr.org peer channel join \
-    -b claytonuniversitychannel.block \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.garyton-university.dl4csr.org peer channel join \
-    -b garytonuniversitychannel.block \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-
-  print_step "Install and instantiate chaincode"
-  docker exec cli.clayton-university.dl4csr.org peer chaincode install \
-    -n dl4csr \
-    -v 1.0.0 \
-    -l golang \
-    -p chaincode \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.garyton-university.dl4csr.org peer chaincode install \
-    -n dl4csr \
-    -v 1.0.0 \
-    -l golang \
-    -p chaincode \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.clayton-university.dl4csr.org peer chaincode instantiate \
-    -o orderer1.dl4csr.org:7050 \
-    -C claytonuniversitychannel \
-    -n dl4csr \
-    -l golang \
-    -v 1.0.0 \
-    -c '{"Args":["init"]}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  echo "Chaincode for clayton-university.dl4csr.org has been instantiated"
-  docker exec cli.garyton-university.dl4csr.org peer chaincode instantiate \
-    -o orderer1.dl4csr.org:7050 \
-    -C garytonuniversitychannel \
-    -n dl4csr \
-    -l golang \
-    -v 1.0.0 \
-    -c '{"Args":["init"]}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  echo "Chaincode for clayton-university.dl4csr.org has been instantiated"
-
-  print_step "Test chaincode invocation"
-  docker exec cli.clayton-university.dl4csr.org peer chaincode invoke \
-    -C claytonuniversitychannel \
-    -n dl4csr \
-    -c '{"Args":["ping"]}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.garyton-university.dl4csr.org peer chaincode invoke \
-    -C garytonuniversitychannel \
-    -n dl4csr \
-    -c '{"Args":["ping"]}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
 }
 
-function remove() {
-  read -r -n 1 -p 'The network and data will be deleted and cannot be recovered, still remove? [y/N] ' FLAG
-  echo
-  if [ "$FLAG" != "y" ]; then
-    echo "Operation canceled"
-    exit 0
-  fi
-  echo "Removing network..."
+function remove_files() {
+  print_step "Remove generated files"
   sudo rm -rf ./channel-artifacts
   sudo rm -rf ./crypto-config
+}
+
+function remove_containers() {
+  print_step "Remove containers"
   docker-compose down --remove-orphans
-  docker system prune --volumes -f
-  images=$(docker images -aq)
-  for image in $images; do
-    docker rmi -f "$image"
-  done
 }
 
-function start() {
-  echo 'Starting network...'
-  docker-compose start
+function install_chaincode() {
+  print_step "Install chaincode"
+  docker exec "$CU_CLI" peer chaincode install -n dl4csr -v "$1" -l golang -p chaincode --tls --cafile "$PK"
+  docker exec "$GU_CLI" peer chaincode install -n dl4csr -v "$1" -l golang -p chaincode --tls --cafile "$PK"
 }
 
-function pause() {
-  echo 'Pausing network...'
-  docker-compose stop
+function instantiate_chaincode() {
+  print_step "Instantiate chaincode"
+  docker exec "$CU_CLI" peer chaincode instantiate -o "$ORDERER" -C "$CU_CHANNEL" -n dl4csr -l golang -v "$1" -c "$INIT_ARGS" --tls --cafile "$PK"
+  sleep 5
+  echo "Chaincode for clayton-university.dl4csr.org has been instantiated"
+  docker exec "$GU_CLI" peer chaincode instantiate -o "$ORDERER" -C "$GU_CHANNEL" -n dl4csr -l golang -v "$1" -c "$INIT_ARGS" --tls --cafile "$PK"
+  sleep 5
+  echo "Chaincode for garyton-university.dl4csr.org has been instantiated"
 }
 
-function upgrade() {
-  docker exec cli.clayton-university.dl4csr.org peer chaincode install \
-    -n dl4csr \
-    -v "$1" \
-    -l golang \
-    -p chaincode \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.clayton-university.dl4csr.org peer chaincode upgrade \
-    -o orderer1.dl4csr.org:7050 \
-    -C claytonuniversitychannel \
-    -n dl4csr \
-    -l golang \
-    -v "$1" \
-    -c '{"Args":["init"]}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
+function upgrade_chaincode() {
+  print_step "Upgrade chaincode"
+  if [ "$1" == "" ]; then
+    echo "Please select a version to update"
+    return
+  fi
+  docker exec "$CU_CLI" peer chaincode upgrade -o "$ORDERER" -C "$CU_CHANNEL" -n dl4csr -l golang -v "$1" -c "$INIT_ARGS" --tls --cafile "$PK"
+  sleep 5
   echo "Chaincode for clayton-university.dl4csr.org has been upgraded"
-  docker exec cli.garyton-university.dl4csr.org peer chaincode install \
-    -n dl4csr \
-    -v "$1" \
-    -l golang \
-    -p chaincode \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
-  docker exec cli.garyton-university.dl4csr.org peer chaincode upgrade \
-    -o orderer1.dl4csr.org:7050 \
-    -C garytonuniversitychannel \
-    -n dl4csr \
-    -l golang \
-    -v "$1" \
-    -c '{"Args":["init"]}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
+  docker exec "$GU_CLI" peer chaincode upgrade -o "$ORDERER" -C "$GU_CHANNEL" -n dl4csr -l golang -v "$1" -c "$INIT_ARGS" --tls --cafile "$PK"
+  sleep 5
   echo "Chaincode for garyton-university.dl4csr.org has been upgraded"
 }
 
-function invoke() {
-  echo 'Testing chaincode invocation...'
+function test_chaincode() {
+  print_step "Test chaincode invocation"
+  docker exec "$CU_CLI" peer chaincode invoke -C "$CU_CHANNEL" -n dl4csr -c "$TEST_ARGS" --tls --cafile "$PK"
+  docker exec "$GU_CLI" peer chaincode invoke -C "$GU_CHANNEL" -n dl4csr -c "$TEST_ARGS" --tls --cafile "$PK"
+}
+
+function invoke_chaincode() {
+  print_step "Invoke chaincode"
   if [ "$2" == "" ]; then
     echo "Usage: launcher.sh $1 [FUNCTION] [ARGUMENTS]"
-    exit 0
+    return
   fi
-  docker exec cli.clayton-university.dl4csr.org peer chaincode invoke \
-    -C claytonuniversitychannel \
-    -n dl4csr \
-    -c '{"Args":['"$2"']}' \
-    --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dl4csr.org/orderers/orderer1.dl4csr.org/msp/tlscacerts/tlsca.dl4csr.org-cert.pem
+  echo "Arguments: $2"
+  docker exec "$CU_CLI" peer chaincode invoke -C "$CU_CHANNEL" -n dl4csr -c '{"Args":['"$2"']}' --tls --cafile "$PK"
+  docker exec "$GU_CLI" peer chaincode invoke -C "$GU_CHANNEL" -n dl4csr -c '{"Args":['"$2"']}' --tls --cafile "$PK"
 }
 
 case $1 in
 "-h" | "--help" | "")
-  show_helps
+  print_helps
   ;;
 "-d" | "--deploy")
-  deploy
+  confirm "Data will lost if the network has been deployed before, still deploy?"
+  print_logo
+  remove_files
+  remove_containers
+  generate_files
+  deploy_network
+  create_channels
+  join_channels
+  install_chaincode "1.0.0"
+  instantiate_chaincode "1.0.0"
+  test_chaincode
   ;;
 "-r" | "--remove")
-  remove
+  confirm "The network and data will be deleted and cannot be recovered, still remove?"
+  remove_files
+  remove_containers
   ;;
 "-s" | "--start")
-  start
+  start_network
   ;;
 "-p" | "--pause")
-  pause
+  stop_network
   ;;
 "-u" | "--upgrade")
-  upgrade "$2"
+  install_chaincode "$2"
+  upgrade_chaincode "$2"
   ;;
 "-i" | "--invoke")
-  builder=""
+  args=""
   index=1
   for arg in "$@"; do
     if [ "$index" != 1 ]; then
-      builder=$builder"\"$arg\""
+      args=$args"\"$arg\""
       if [ "$index" -lt $# ]; then
-        builder=$builder","
+        args=$args","
       fi
     fi
     ((index = "$index" + 1))
   done
-  invoke "$1" "$builder"
+  invoke_chaincode "$1" "$args"
   ;;
 *)
   echo "Unknown flag: $1"
   echo 'Use with flag "--help" or "-h" to show helps'
   ;;
 esac
+echo
