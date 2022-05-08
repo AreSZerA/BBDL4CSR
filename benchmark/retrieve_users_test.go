feb2045ded0d3cb6559ecd767b44ec7574a59165
AreSZerA <18722064@bjtu.edu.cn>
2022-05-08 19:01:42 +0800

    Update benchmark records

--- benchmark/retrieve_users_test.go
+++ benchmark/retrieve_users_test.go
@@ -7,12 +7,21 @@ import (
 
 func init() {
 	// Call initUsers(n) to insert n users.
-	//initUsers(10000)
+	//initUsers(1000)
 }
 
 func BenchmarkRetrieveUsers(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := Query(FuncRetrieveUserByEmail, []byte("admin@dl4csr.org"))
+		if err != nil {
+			b.FailNow()
+		}
+	}
+}
+
+func BenchmarkRetrieveRegex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := Query(FuncRetrieveUsersByQuery, []byte(`{"selector":{"user_email":{"$regex":".*?dl4csr.*?"}}}`))
 		if err != nil {
 			b.FailNow()
 		}
