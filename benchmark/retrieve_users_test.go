beda7661ac97b299f1b809ba987706903e342e5d
AreSZerA <18722064@bjtu.edu.cn>
2022-05-09 16:31:14 +0800

    Remove benchmark results, add benchmark method

--- benchmark/retrieve_users_test.go
+++ benchmark/retrieve_users_test.go
@@ -7,10 +7,10 @@ import (
 
 func init() {
 	// Call initUsers(n) to insert n users.
-	//initUsers(1000)
+	initUsers(1000)
 }
 
-func BenchmarkRetrieveUsers(b *testing.B) {
+func BenchmarkRetrieveByCompositeKet(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := Query(FuncRetrieveUserByEmail, []byte("admin@dl4csr.org"))
 		if err != nil {
@@ -19,7 +19,16 @@ func BenchmarkRetrieveUsers(b *testing.B) {
 	}
 }
 
-func BenchmarkRetrieveRegex(b *testing.B) {
+func BenchmarkRetrieveByQuery(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := Query(FuncRetrieveUsersByQuery, []byte(`{"selector":{"user_email":"admin@dl4csr.org"}}`))
+		if err != nil {
+			b.FailNow()
+		}
+	}
+}
+
+func BenchmarkRetrieveByQueryRegexp(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := Query(FuncRetrieveUsersByQuery, []byte(`{"selector":{"user_email":{"$regex":".*?dl4csr.*?"}}}`))
 		if err != nil {
